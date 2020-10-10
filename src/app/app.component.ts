f1215c14031961fcfd58d073308ce9b7a8c69e6d
David <compresdavid@gmail.com>
2020-10-10 18:14:40 +0200

    fix: add test logos + headless chrome test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+
   title = 'hmi-seat';
 
   showVideo = false;
+
   showKombi = false;
 
-  toggleVideo(newValue: boolean): void{
+  toggleVideo(newValue: boolean): void {
     this.showVideo = newValue;
   }
 
-  toggleKombi(show: boolean): void{
+  toggleKombi(show: boolean): void {
     this.showKombi = show;
   }
 }
