2c97c9a40522f381cef8be6f5978ab64b132291e
David G <compresdavid@gmail.com>
2020-10-15 11:37:35 +0200

    fix: delete chronology component + carousel fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,9 @@ import { NavbarLogoComponent } from './navbar-logo/navbar-logo.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { WelcomeVideoComponent } from './welcome-video/welcome-video.component';
 import { KombiComponent } from './kombi/kombi.component';
-import { ChronologyComponent } from './chronology/chronology.component';
 import { FutureComponent } from './future/future.component';
-import {CarouselTimelineComponent} from './carousel-timeline/carousel.component';
+import { CarouselTimelineComponent } from './carousel-timeline/carousel.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 
 @NgModule({
   declarations: [
@@ -19,15 +17,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     WelcomeComponent,
     WelcomeVideoComponent,
     KombiComponent,
-    ChronologyComponent,
     FutureComponent,
-    CarouselTimelineComponent
+    CarouselTimelineComponent,
   ],
-  imports: [
-    BrowserModule,
-    BrowserAnimationsModule
-  ],
+  imports: [BrowserModule, BrowserAnimationsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/app/carousel-timeline/carousel.component.ts
+++ src/app/carousel-timeline/carousel.component.ts
@@ -1,40 +1,47 @@
-import { IfStmt } from '@angular/compiler';
 import { AfterViewInit } from '@angular/core';
-import { Component, EventEmitter, OnInit, Input, QueryList, ViewChild, ViewChildren, ElementRef, HostListener, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Input,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+  ElementRef,
+  Output,
+} from '@angular/core';
 import { ICarouselItem } from './Icarousel-item.metadata';
 
 @Component({
   selector: 'app-carousel-timeline',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
 })
 export class CarouselTimelineComponent implements OnInit, AfterViewInit {
-
   @ViewChild('contentItem') contentItem: ElementRef;
   @ViewChild('stepsEvents') stepsEvents: ElementRef;
   @ViewChild('fillingLine') fillingLine: ElementRef;
   @ViewChildren('containerEvents') containerEvents: QueryList<ElementRef>;
   @ViewChildren('timelineEvents') timelineEvents: QueryList<ElementRef>;
 
-
   @Input() height = 500;
   @Input() isFullScreen = false;
   @Input() items: ICarouselItem[] = [];
   @Output() clickNext = new EventEmitter<boolean>();
 
-
-
   public finalHeight: string | number = 0;
   public currentPosition = 0;
-    public screenWidth: any;
-
-
+  public screenWidth: any;
 
   constructor() {
     this.finalHeight = this.isFullScreen ? '100vh' : `${this.height}px`;
-   }
+  }
 
-  private static setTransformValue(element: any, property: any, value: any) {
+  private static setTransformValue(
+    element: any,
+    property: any,
+    value: any
+  ): void {
     element.style['-webkit-transform'] = property + '(' + value + ')';
     element.style['-moz-transform'] = property + '(' + value + ')';
     element.style['-ms-transform'] = property + '(' + value + ')';
@@ -48,36 +55,35 @@ export class CarouselTimelineComponent implements OnInit, AfterViewInit {
 
   ngAfterViewInit(): void {
     this.screenWidth = window.innerWidth;
-    // console.log(this.screenWidth);
-    // this.initTimeline();
-
   }
 
-  ngOnInit() {
-    this.items.map( ( i, index ) => {
+  ngOnInit(): void {
+    this.items.map((i, index) => {
       i.id = index;
       i.marginLeft = 0;
     });
-
   }
 
-  setCurrentPosition(position: number) {
-    const timelineEventsArray = this.timelineEvents.toArray();
+  setCurrentPosition(position: number): void {
     this.currentPosition = position;
-    this.items.find(i => i.id === 0).marginLeft = -100 * position;
-
-    if (this.currentPosition === this.items.length - 1 ){
-      console.log(this.items[position].id );
-      this.clickNext.emit(true);
+    this.items.find((i) => i.id === 0).marginLeft = -100 * position;
+    if (this.currentPosition >= this.items.length - 1) {
+      const queryElement = document.querySelector('.dsc-carousel');
+      queryElement.classList.add('animate__fadeOut');
+      queryElement.addEventListener('animationend', () => {
+        queryElement.classList.remove('animate__fadeOut');
+        this.clickNext.emit(true);
+      });
+    } else {
+      this.updateTimelinePosition(this.currentPosition);
+      this.addStyleToContent(this.currentPosition);
     }
-    this.updateTimelinePosition(this.currentPosition);
-    this.addStyleToContent(this.currentPosition);
   }
 
-  addStyleToContent(currentPosition: number){
+  addStyleToContent(currentPosition: number): void {
     const containerEventsArray = this.containerEvents.toArray();
-    let currentPos = currentPosition;
-    let lastPos = currentPosition === 0 ? 1 : currentPosition - 1;
+    const currentPos = currentPosition;
+    const lastPos = currentPosition === 0 ? 1 : currentPosition - 1;
 
     // }
     // containerEventsArray[lastPos].nativeElement.style.filter = ` hue-rotate(210deg)`;
@@ -85,39 +91,44 @@ export class CarouselTimelineComponent implements OnInit, AfterViewInit {
     //       containerEventsArray[currentPos].nativeElement.style.filter = ` hue-rotate(0deg))`;
     // }
     containerEventsArray[lastPos].nativeElement.style.filter = `blur(9px)`;
-    if (containerEventsArray[currentPos].nativeElement.style.filter === `blur(9px)`){
-          containerEventsArray[currentPos].nativeElement.style.filter = `blur(0px)`;
+    if (
+      containerEventsArray[currentPos].nativeElement.style.filter ===
+      `blur(9px)`
+    ) {
+      containerEventsArray[currentPos].nativeElement.style.filter = `blur(0px)`;
     }
     // containerEventsArray[lastPos].nativeElement.style.filter = `grayscale(1)`;
     // if (containerEventsArray[currentPos].nativeElement.style.filter === `grayscale(1)`){
     //       containerEventsArray[currentPos].nativeElement.style.filter = `grayscale(0)`;
     // }
+  }
 
- }
-
-
-  updateTimelinePosition(currentPosition: number) {
+  updateTimelinePosition(currentPosition: number): void {
     const timelineEventsArray = this.timelineEvents.toArray();
     const widthParent = this.stepsEvents.nativeElement.clientWidth;
-    let currentPos = currentPosition;
-    let lastPos = currentPosition === 0 ? 1 : currentPosition - 1;
+    const currentPos = currentPosition;
+    const lastPos = currentPosition === 0 ? 1 : currentPosition - 1;
 
     let widthBetweenEvents: number;
-      widthBetweenEvents = CarouselTimelineComponent.itemDiff(timelineEventsArray[currentPos].nativeElement.offsetLeft, timelineEventsArray[lastPos].nativeElement.offsetLeft);
+    widthBetweenEvents = CarouselTimelineComponent.itemDiff(
+      timelineEventsArray[currentPos].nativeElement.offsetLeft,
+      timelineEventsArray[lastPos].nativeElement.offsetLeft
+    );
 
-      let result: number;
-      result = ( widthBetweenEvents / widthParent ) * currentPos;
-      this.translateTimeline(result);
+    let result: number;
+    result = (widthBetweenEvents / widthParent) * currentPos;
+    this.translateTimeline(result);
   }
 
-  translateTimeline(value: number) {
-    CarouselTimelineComponent.setTransformValue(this.fillingLine.nativeElement, 'scaleX', value);
+  translateTimeline(value: number): void {
+    CarouselTimelineComponent.setTransformValue(
+      this.fillingLine.nativeElement,
+      'scaleX',
+      value
+    );
   }
 
-
-
-
-  setNext() {
+  setNext(): void {
     let finalPercentage = 0;
     let nextPosition = this.currentPosition + 1;
     if (nextPosition <= this.items.length - 1) {
@@ -125,22 +136,20 @@ export class CarouselTimelineComponent implements OnInit, AfterViewInit {
     } else {
       nextPosition = 0;
     }
-    this.items.find(i => i.id === 0).marginLeft = finalPercentage;
+    this.items.find((i) => i.id === 0).marginLeft = finalPercentage;
     this.currentPosition = nextPosition;
   }
 
-  setBack() {
+  setBack(): void {
     let finalPercentage = 0;
-    let backPosition = this.currentPosition  - 1;
+    let backPosition = this.currentPosition - 1;
     if (backPosition >= 0) {
       finalPercentage = -100 * backPosition;
     } else {
       backPosition = this.items.length - 1;
       finalPercentage = -100 * backPosition;
     }
-    this.items.find(i => i.id === 0).marginLeft = finalPercentage;
+    this.items.find((i) => i.id === 0).marginLeft = finalPercentage;
     this.currentPosition = backPosition;
-
   }
-
 }
